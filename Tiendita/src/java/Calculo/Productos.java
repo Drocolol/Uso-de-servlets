939b6629dcabe1bc29fec93143ed47217a17fa1c
TorresDiegoLeo4IV9 <125711723+TorresDiegoLeo4IV9@users.noreply.github.com>
2023-03-19 22:31:05 -0600

    Add files via upload

---
+++ Tiendita/src/java/Calculo/Productos.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculo;
+
+/**
+ *
+ * @author 52556
+ */
+public class Productos {
+    String producto;
+    int precio;
+
+    public Productos() {
+        this.producto = producto;
+        this.precio = precio;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
+}
---
+++ Tiendita/src/java/Calculo/Usuario.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Calculo;
+
+/**
+ *
+ * @author 52556
+ */
+public class Usuario {
+    
+    private String cliente,email;
+
+    public Usuario() {
+        this.cliente = "";
+        this.email = "";
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
